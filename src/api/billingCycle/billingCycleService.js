d889be609f522645824494872e84688948341480
Luciano Oliveira <luciano101@msn.com>
2018-03-04 09:48:21 -0500

    add count route to return count of objects and middleware to return specific error

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle');
 BillingCycle.methods(['get', 'put', 'post', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error){
+            res.status(500).json({errors: [error]});
+        } else {
+            res.json({value});
+        }
+    });
+});
+
 module.exports = BillingCycle;
