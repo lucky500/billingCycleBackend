e225c59dcd715cfd8939620736a3077aac6b2be7
Luciano Oliveira <luciano101@msn.com>
2018-03-04 09:15:30 -0500

    testing API and add custom error messages to mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://localhost/mymoney');
 
-//adding your own mongoose error messages
+//adding custom mongoose error messages
 mongoose.Error.messages.Number.max = "The value '{VALUE}' is greater than the '{MAX}', please correct it."
