ebda7e9834ebfca94790bd230730c1a8c81df899
Luciano Oliveira <luciano101@msn.com>
2018-03-04 12:50:44 -0500

    add aggregate and group methods to return sum of credits and debts

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -13,4 +13,30 @@ BillingCycle.route('count', (req, res, next) => {
     });
 });
 
+BillingCycle.route('summary', (req, res, next) => {
+    //mongo docs aggregate groups values from multiple documents and return single value
+    BillingCycle.aggregate({
+        //$projects, passes along the documents with the requested fields to the next stage in the pipeline.
+        //$sum - calculates and returns the sum of numeric values.
+        $project: { credit: { $sum: "$credits.value" }, debt: {$sum: "$debts.value" }}
+    }, {
+        //group based in a criteria, _id is mandatory, so we set it to null, with id
+        //we are grouping based on year or month, but we want to group everything
+        //without a criteria, so we are setting _id to null
+        $group: {_id: null, credit: { $sum: "$credit" }, debt: {$sum: "$debt"}}
+    }, {
+        //projecting our results, setting _id to 0, and grabbing the
+        //result of our credit and debit.
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }, (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 })
+        }
+    });
+});
+
+
+
 module.exports = BillingCycle;
