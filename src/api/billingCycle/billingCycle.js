1ec32963e2e77a0857b296231416c39524747979
Luciano Oliveira <luciano101@msn.com>
2018-03-04 09:15:12 -0500

    testing API and add custom error messages to mongoose

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -1,5 +1,5 @@
 const restful = require('node-restful');
-const moongose = restful.mongoose;
+const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -8,12 +8,12 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Please inform debt amount'] },
     status: { type: String, required: false, uppercase: true, 
               enum: ['PAID', 'PENDING', 'SCHEDULED']}
 });
 
-const billingCycleSchema = new moongose.Schema({
+const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://localhost/mymoney');
+
+//adding your own mongoose error messages
+mongoose.Error.messages.Number.max = "The value '{VALUE}' is greater than the '{MAX}', please correct it."
